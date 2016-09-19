897bd47b851ea2a157a5f41037f1e014d994502b
Keunhong Lee <dlrmsghd@gmail.com>
2016-09-19 09:01:43 +0000

    Generated from: e397ebca8ad07c723e1652894b6f6e7dfc264833

--- src/Networking/E_Link.cpp
+++ src/Networking/E_Link.cpp
@@ -208,8 +208,8 @@ void Link::addPort(Port* port)
 
 struct pcap_file_header {
 	uint32_t magic;
-	u_short version_major;
-	u_short version_minor;
+	uint16_t version_major;
+	uint16_t version_minor;
 	uint32_t thiszone;     /* gmt to local correction */
 	uint32_t sigfigs;    /* accuracy of timestamps */
 	uint32_t snaplen;    /* max length saved portion of each pkt */
